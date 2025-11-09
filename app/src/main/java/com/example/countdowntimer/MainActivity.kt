62efac18b065d667304e9f87a30c87d82c684560
David Godfrey <kinqvidda@gmail.com>
2025-11-09 21:40:04 +0100

    Initial commit

---
+++ app/src/main/java/com/example/countdowntimer/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.countdowntimer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.countdowntimer.ui.theme.CountDownTimerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CountDownTimerTheme {
+
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CountDownTimerTheme {
+       
+    }
+}
